8ac10ceac5b7fded413b1c69c410fa90da4dc6da
Hanxuan Yu <63954268+phos2000@users.noreply.github.com>
2024-02-13 00:42:55 -0600

    created CPP, updated NHANES

---
+++ ASCVD_RIPS.cpp
@@ -0,0 +1,454 @@
+#include <Rcpp.h>
+#include <iostream>
+#include <cmath>
+#include <cerrno>
+#include <cfenv>
+#include <cstring>
+#include <algorithm>
+#include <fstream>
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+// Function to convert rates to probabilities
+double RateToProb(double rate, double t){
+  return(1-exp(-rate*t));
+}
+
+// [[Rcpp::export]]
+// Function to convert probabilities to rates
+double ProbToRate(double prob, double t){
+  return(-log(1-prob)/t);
+}
+
+// [[Rcpp::export]]
+double ProbToProb(double prob_t, double t) {
+  double P1 = 1 - pow((1 - prob_t), 1/t);
+  return P1;
+}
+
+// [[Rcpp::export]]
+// Function to convert Odds Ratio to Relative Risk which is prob
+double OR_to_RR(double OR, double prob){
+  return(OR/(1-prob + prob*OR));
+}
+
+NumericVector rgompertz(int n, double shape, double rate){
+  // calling rgompertz()
+  Function f("rgompertz");
+  return f(n, shape, rate);
+}
+
+NumericVector rEXP(int n, double rate){
+  // calling rexp()
+  Function f("rexp");
+  return f(n, rate);
+}
+
+NumericVector cHealthyAve(double age_mod, double delta_time){
+  Function f("cHealthyAve");
+  return f(age_mod, delta_time);
+}
+
+double discount(double disc_rate, double start_time, double delta_time){
+  // from 0 to start_time
+  // from 0 to end_time
+  return (1-exp(-disc_rate*(start_time+delta_time)))/disc_rate - 
+    (1-exp(-disc_rate*start_time))/disc_rate;
+}
+
+// [[Rcpp::export]] 
+double ttGompertzDeath(double age_mod, bool male, int type, 
+                       NumericVector parameters_f_shape, NumericVector parameters_f_rate, 
+                       NumericVector parameters_m_shape, NumericVector parameters_m_rate,
+                       NumericVector parameters_f_hr_shape, NumericVector parameters_f_hr_rate, 
+                       NumericVector parameters_m_hr_shape,NumericVector parameters_m_hr_rate){
+  //output 
+  double ttdeath;
+  
+  if (round(age_mod) > 100) {return 0;}
+  if (type == 1){
+    if (male){
+      ttdeath = rgompertz(1, parameters_m_shape(round(age_mod)), 
+                             parameters_m_rate(round(age_mod)))[0]; 
+    } else {
+      ttdeath = rgompertz(1, parameters_f_shape(round(age_mod)), 
+                             parameters_f_rate(round(age_mod)))[0]; 
+    }
+  } else {
+    if (male){
+      ttdeath = rgompertz(1, parameters_m_hr_shape(round(age_mod)), 
+                             parameters_m_hr_rate(round(age_mod)))[0]; 
+    } else {
+      ttdeath = rgompertz(1, parameters_f_hr_shape(round(age_mod)), 
+                             parameters_f_hr_rate(round(age_mod)))[0]; 
+    }
+  }
+  return(ttdeath);
+}
+
+// [[Rcpp::export]]
+// Function for a 10-yr-horizon DES model for ASCVD
+List mod_main(int n_pop, bool strategy, int horizon,
+              double disc_rate,
+              NumericVector age_vec, NumericVector male_vec, NumericVector race_vec,
+              NumericVector diabetes_vec, NumericVector ldl_chol_vec,
+              NumericVector pce_orig_vec, NumericVector pce_prs_vec,
+              double rrStatinsASCVD,
+              double pFatalASCVD_m_young, double pFatalASCVD_m_old,
+              double pFatalASCVD_f_young, double pFatalASCVD_f_old,
+              double pMildAdverse, double pMajorAdverse, 
+              double pMajorAdverseBeingFatal,
+              NumericVector parameters_f_shape, NumericVector parameters_f_rate, 
+              NumericVector parameters_m_shape, NumericVector parameters_m_rate,
+              NumericVector parameters_f_hr_shape, NumericVector parameters_f_hr_rate, 
+              NumericVector parameters_m_hr_shape,NumericVector parameters_m_hr_rate,
+              double uDeath, double uHealthy, double uAfterASCVD,
+              double uHealthyStatin, double uPenaltyMajorAdverse, 
+              double uPenaltyMildAdverse,
+              double cAnnualStatin, double cAnnualFU, double cFatalASCVD, 
+              double cNonFatalASCVD, double cMildAdverse, double cMajorAdverse){
+  //output
+  NumericVector LE_vec(n_pop);
+  NumericVector LE_disc_vec(n_pop);
+  NumericVector QALE_vec(n_pop);
+  NumericVector QALE_disc_vec(n_pop);
+  NumericVector cost_vec(n_pop);
+  NumericVector cost_disc_vec(n_pop);
+  NumericVector mild_adverse_vec(n_pop);
+  NumericVector major_adverse_vec(n_pop);
+  NumericVector major_adverse_fatal_vec(n_pop);
+  NumericVector ASCVD_fatal_vec(n_pop);
+  NumericVector event_death_vec(n_pop); // 1 for happening, 0 for none.
+  NumericVector ttevent_death_vec(n_pop);
+  NumericVector event_bg_death_vec(n_pop);
+  NumericVector ttevent_bg_death_vec(n_pop);
+  NumericVector event_ASCVD_vec(n_pop);
+  NumericVector ttevent_ASCVD_vec(n_pop);
+  NumericVector event_ASCVD_death_vec(n_pop);
+  NumericVector ttevent_ASCVD_death_vec(n_pop);
+  
+  //looping over population
+  for(int id = 0; id < n_pop; id++){
+    //Read in individual characteristics
+    double age_ini = age_vec(id); //initial age
+    double age_mod = age_ini;
+    bool male   = male_vec(id);
+    bool race = race_vec(id);
+    int ldl_chol = ldl_chol_vec(id);
+    bool diabetes = diabetes_vec(id);
+    bool pce_orig = pce_orig_vec(id);
+    bool pce_prs = pce_prs_vec(id);
+    
+    //Define tracker variables
+    char state = 'H';
+    double time = 0; // now()
+    double delta_time = 0; // the time staying in a state
+    double tthorizon = horizon - time;
+    double pASCVD = pce_prs; // may update later in a longer horizon
+    bool statins = 0;
+    double uStatins = uHealthyStatin;
+    
+    // Initialize statin treatment 
+    //strategy - 1 for PRS, 0 for status quo
+    if (ldl_chol >= 190 | diabetes == 1) {
+      statins = 1;
+    } else if (strategy){
+      if (pce_prs >= 0.075){statins = 1;}
+    } else if (pce_orig >= 0.075){statins = 1;}
+    
+    if (statins) {
+      
+      // adverse event
+      if (R::unif_rand()< pMajorAdverse){
+        major_adverse_vec(id) = 1;
+        if (R::unif_rand() < pMajorAdverseBeingFatal){
+          
+          delta_time = 0;
+          
+          state = 'D'; // ever change state, calculate all the cost and utility.
+          //LE
+          LE_vec(id) += delta_time;
+          LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+          //one-time cost
+          cost_vec(id) += cMajorAdverse;
+          cost_disc_vec(id) += cMajorAdverse / pow(1.0 + disc_rate, time);
+          //Utility
+          QALE_vec(id) += uDeath * delta_time;
+          QALE_disc_vec(id) += uDeath * discount(disc_rate, time, delta_time);
+          
+          //age, time, event records
+          age_mod += delta_time;
+          time += delta_time;
+          major_adverse_fatal_vec(id) = 1;
+          event_death_vec(id) = 1;
+          ttevent_death_vec(id) = time;
+          
+          
+          
+        } else {
+          uStatins = uHealthyStatin - uPenaltyMajorAdverse;
+          //one-time cost
+          cost_vec(id) += cMajorAdverse;
+          cost_disc_vec(id) += cMajorAdverse / pow(1.0 + disc_rate, time);
+          
+        }
+      } else if (unif_rand()< pMildAdverse){
+        mild_adverse_vec(id) = 1;
+        uStatins = uHealthyStatin - uPenaltyMildAdverse;
+        //one-time cost
+        cost_vec(id) += cMildAdverse;
+        cost_disc_vec(id) += cMildAdverse / pow(1.0 + disc_rate, time);
+      }
+    }
+    
+    // Define the background death time
+    double ttBGdeath = std::min(100 - age_mod, ttGompertzDeath(age_mod, male, 1, 
+                                                               parameters_f_shape, parameters_f_rate, 
+                                                               parameters_m_shape,parameters_m_rate,
+                                                               parameters_f_hr_shape, parameters_f_hr_rate, 
+                                                               parameters_m_hr_shape, parameters_m_hr_rate));
+    
+    // Healthy State, including statin treatment
+    if (state == 'H'){
+      // time to ASCVD
+      // consider PRS_revised PCE to be the true ASCVD risk. 
+      if(statins){
+        //relative risk
+        pASCVD = rrStatinsASCVD*pASCVD;
+      }
+      // 10-year prob to 1-year rate
+      double ttASCVD = rEXP(1, ProbToRate(pASCVD, 10))[0];
+      
+      // next event: BG_death or ASCVD?
+      if (ttASCVD < ttBGdeath & ttASCVD < tthorizon){
+        
+        delta_time = ttASCVD;
+        
+        state = 'S';
+        //LE
+        LE_vec(id) += delta_time;
+        LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+        
+        double cHealthy = cHealthyAve(age_mod, delta_time)[0];
+        if (statins){
+          //cost
+          cHealthy += cAnnualStatin;
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uStatins * delta_time;
+          QALE_disc_vec(id) += uStatins * discount(disc_rate, time, delta_time);
+        } else {
+          //cost
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uHealthy * delta_time;
+          QALE_disc_vec(id) += uHealthy * discount(disc_rate, time, delta_time);
+        }
+        
+        //age, time, event records
+        age_mod += delta_time;
+        time += delta_time;
+        event_ASCVD_vec(id) = 1;
+        ttevent_ASCVD_vec(id) = time;
+        
+        
+        
+        // ever new state while not break, update time-to-event starting point
+        ttBGdeath = ttBGdeath - delta_time;
+        tthorizon = tthorizon - delta_time;
+        
+      }  else if (ttBGdeath <= ttASCVD & ttBGdeath < tthorizon){
+        
+        delta_time = ttBGdeath;
+        
+        state = 'D';
+        //LE
+        LE_vec(id) += delta_time;
+        LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+        
+        double cHealthy = cHealthyAve(age_mod, delta_time)[0];
+        if (statins){
+          //cost
+          cHealthy += cAnnualStatin;
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uStatins * delta_time;
+          QALE_disc_vec(id) += uStatins * discount(disc_rate, time, delta_time);
+        } else {
+          //cost
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uHealthy * delta_time;
+          QALE_disc_vec(id) += uHealthy * discount(disc_rate, time, delta_time);
+        }
+        
+        //age, time, event records
+        age_mod += delta_time;
+        time += delta_time;
+        event_death_vec(id) = 1;
+        ttevent_death_vec(id) = time;
+        event_bg_death_vec(id) = 1;
+        ttevent_bg_death_vec(id) = time;
+        
+        continue;
+        
+      }  else if (ttBGdeath >= tthorizon & ttASCVD >= tthorizon){
+        
+        delta_time = tthorizon;
+        
+        //LE
+        LE_vec(id) += delta_time;
+        LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+        
+        double cHealthy = cHealthyAve(age_mod, delta_time)[0];
+        if (statins){
+          //cost
+          cHealthy += cAnnualStatin;
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uStatins * delta_time;
+          QALE_disc_vec(id) += uStatins * discount(disc_rate, time, delta_time);
+        } else {
+          //cost
+          cost_vec(id) += cHealthy * delta_time;
+          cost_disc_vec(id) += cHealthy * discount(disc_rate, time, delta_time);
+          //Utility
+          QALE_vec(id) += uHealthy * delta_time;
+          QALE_disc_vec(id) += uHealthy * discount(disc_rate, time, delta_time);
+        }
+        
+        //age, time, event records
+        age_mod += delta_time;
+        time += delta_time;
+        event_death_vec(id) = 0;
+        ttevent_death_vec(id) = time;
+
+        continue; // arrive horizon
+      }
+    }
+    
+    // ASCVD State
+    // possible events: BG death, death in a year
+    if (state == 'S'){
+      
+      // prob of fatal ASCVD event
+      double pFatalASCVD;
+      
+      if (male){
+        if (age_mod<65){
+          pFatalASCVD = pFatalASCVD_m_young;
+        } else {pFatalASCVD = pFatalASCVD_m_old;}
+      } else {
+        if (age_mod<65){
+          pFatalASCVD = pFatalASCVD_f_young;
+        } else {pFatalASCVD = pFatalASCVD_f_old;}
+      }
+     
+     if (R::unif_rand() < pFatalASCVD){
+       delta_time = 0;
+       state = 'D';
+       
+       //LE
+       LE_vec(id) += delta_time;
+       LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+       //one-time cost
+       cost_vec(id) += cFatalASCVD;
+       cost_disc_vec(id) += cFatalASCVD / pow(1.0 + disc_rate, time);
+       //Utility
+       QALE_vec(id) += uAfterASCVD * delta_time;
+       QALE_disc_vec(id) += uAfterASCVD * discount(disc_rate, time, delta_time);
+       
+       //age, time, event records
+       age_mod += delta_time;
+       time += delta_time;
+       ASCVD_fatal_vec(id) = 1;
+       event_death_vec(id) = 1;
+       ttevent_death_vec(id) = time;
+       event_ASCVD_death_vec(id) = 1;
+       ttevent_ASCVD_death_vec(id) = time;
+       
+       continue;
+       
+     } else {
+       
+       // nonFatal ASCVD
+       ASCVD_fatal_vec(id) = 0;
+       
+       // time to post-ASCVD death
+       double ttPostASCVDdeath = ttGompertzDeath(age_mod, male, 2, 
+                                                 parameters_f_shape, parameters_f_rate, 
+                                                 parameters_m_shape,parameters_m_rate,
+                                                 parameters_f_hr_shape, parameters_f_hr_rate, 
+                                                 parameters_m_hr_shape, parameters_m_hr_rate);
+       
+       // next event: BG_death or post-ASCVD death, or horizon?
+       delta_time = std::min({ttPostASCVDdeath, ttBGdeath, tthorizon});
+       
+       //LE
+       LE_vec(id) += delta_time;
+       LE_disc_vec(id) += discount(disc_rate, time, delta_time);
+       //one-time cost
+       cost_vec(id) += cNonFatalASCVD;
+       cost_disc_vec(id) += cNonFatalASCVD / pow(1.0 + disc_rate, time);
+       //continuous cost
+       cost_vec(id) += cAnnualFU * delta_time;
+       cost_disc_vec(id) += cAnnualFU * discount(disc_rate, time, delta_time);
+       //Utility
+       QALE_vec(id) += uAfterASCVD * delta_time;
+       QALE_disc_vec(id) += uAfterASCVD * discount(disc_rate, time, delta_time);
+       
+       //age, time
+       age_mod += delta_time;
+       time += delta_time;
+       
+       //event records
+       if (delta_time == ttPostASCVDdeath){
+         state = 'D';
+         event_death_vec(id) = 1;
+         ttevent_death_vec(id) = time;
+         event_ASCVD_death_vec(id) = 1;
+         ttevent_ASCVD_death_vec(id) = time;
+       } else if(delta_time == ttBGdeath) {
+         state = 'D';
+         event_death_vec(id) = 1;
+         ttevent_death_vec(id) = time;
+         event_bg_death_vec(id) = 1;
+         ttevent_bg_death_vec(id) = time;
+       } else {
+         // arrive horizon
+         event_death_vec(id) = 0;
+         ttevent_death_vec(id) = time;
+       }
+       continue;
+     }
+    } 
+    // Death State 
+    if (state == 'D') continue;
+  }
+  
+  return(List::create(
+      Named("LE")        = LE_vec,
+      Named("LE_disc")   = LE_disc_vec,
+      Named("QALE")      = QALE_vec,
+      Named("QALE_disc") = QALE_disc_vec,
+      Named("cost")      = cost_vec,
+      Named("cost_disc") = cost_disc_vec,
+      Named("mild_adverse") = mild_adverse_vec,
+      Named("major_adverse") = major_adverse_vec,
+      Named("fatal_major_adverse") = major_adverse_fatal_vec,
+      Named("fatal_ASCVD") = ASCVD_fatal_vec,
+      Named("death") = event_death_vec,
+      Named("ttdeath") = ttevent_death_vec,
+      Named("BGdeath") = event_bg_death_vec,
+      Named("ttBGdeath") = ttevent_bg_death_vec,
+      Named("ASCVD") = event_ASCVD_vec,
+      Named("ttASCVD") = ttevent_ASCVD_vec,
+      Named("ASCVDdeath") = event_ASCVD_death_vec,
+      Named("ttASCVDdeath") = ttevent_ASCVD_death_vec));
+}
--- building_nhanes_sample.R
+++ building_nhanes_sample.R
@@ -12,7 +12,6 @@
 # Read SAS -----------------------------------------------------------------------
 
 ## Set working directory for output
-# setwd("./NHANES SAS data")
 
 ## Read SAS files and pull relevant columns into dataframe
   # 2017-2018
@@ -25,8 +24,6 @@
   nhanes_data_7 = data.frame(read_xpt("NHANES SAS data/MCQ_J.XPT", col_select = c("SEQN","MCQ160D","MCQ160F","MCQ160E")))
   nhanes_data_8 = data.frame(read_xpt("NHANES SAS data/SMQ_J.XPT", col_select = c("SEQN","SMQ040")))
   nhanes_data_9 = data.frame(read_xpt("NHANES SAS data/TCHOL_J.XPT", col_select = c("SEQN","LBXTC")))
-  # selecting only antihypertensive medications from prescription medications file
-  nhanes_data_10 = data.frame(read_xpt("NHANES SAS data/RXQ_RX_J.XPT", col_select = c("SEQN","RXDRSC1")))
   nhanes_data_11 = data.frame(read_xpt("NHANES SAS data/BPQ_J.XPT", col_select = c("SEQN", "BPQ050A")))
   nhanes_data_12 = data.frame(read_xpt("NHANES SAS data/TRIGLY_J.XPT", col_select = c("SEQN", "LBDLDL")))
   
@@ -40,8 +37,6 @@
   nhanes_data_7A = data.frame(read_xpt("NHANES SAS data/MCQ_H.XPT", col_select = c("SEQN","MCQ160D","MCQ160F","MCQ160E")))
   nhanes_data_8A = data.frame(read_xpt("NHANES SAS data/SMQ_H.XPT", col_select = c("SEQN","SMQ040")))
   nhanes_data_9A = data.frame(read_xpt("NHANES SAS data/TCHOL_H.XPT", col_select = c("SEQN","LBXTC")))
-  # selecting only antihypertensive medications from prescription medications file
-  nhanes_data_10A = data.frame(read_xpt("NHANES SAS data/RXQ_RX_H.XPT", col_select = c("SEQN","RXDRSC1")))
   nhanes_data_11A = data.frame(read_xpt("NHANES SAS data/BPQ_H.XPT", col_select = c("SEQN", "BPQ050A")))
   nhanes_data_12A = data.frame(read_xpt("NHANES SAS data/TRIGLY_H.XPT", col_select = c("SEQN", "LBDLDL")))
   
@@ -55,18 +50,16 @@
   nhanes_data_7B = data.frame(read_xpt("NHANES SAS data/MCQ_I.XPT", col_select = c("SEQN","MCQ160D","MCQ160F","MCQ160E")))
   nhanes_data_8B = data.frame(read_xpt("NHANES SAS data/SMQ_I.XPT", col_select = c("SEQN","SMQ040")))
   nhanes_data_9B = data.frame(read_xpt("NHANES SAS data/TCHOL_I.XPT", col_select = c("SEQN","LBXTC")))
-  # selecting only antihypertensive medications from prescription medications file
-  nhanes_data_10B = data.frame(read_xpt("NHANES SAS data/RXQ_RX_I.XPT", col_select = c("SEQN","RXDRSC1")))
   nhanes_data_11B = data.frame(read_xpt("NHANES SAS data/BPQ_I.XPT", col_select = c("SEQN", "BPQ050A")))
   nhanes_data_12B = data.frame(read_xpt("NHANES SAS data/TRIGLY_I.XPT", col_select = c("SEQN", "LBDLDL")))
   
 ## Merge dataframes and rename columns
   nhanes_raw_data_C <- list(nhanes_data_1,nhanes_data_2,nhanes_data_3,nhanes_data_4,
-                          nhanes_data_5,nhanes_data_6,nhanes_data_7,nhanes_data_8,nhanes_data_9,nhanes_data_10, nhanes_data_11, nhanes_data_12)  
+                          nhanes_data_5,nhanes_data_6,nhanes_data_7,nhanes_data_8,nhanes_data_9, nhanes_data_11, nhanes_data_12)  
   nhanes_raw_data_A <- list(nhanes_data_1A,nhanes_data_2A,nhanes_data_3A,nhanes_data_4A,
-                          nhanes_data_5A,nhanes_data_6A,nhanes_data_7A,nhanes_data_8A,nhanes_data_9A,nhanes_data_10A, nhanes_data_11A, nhanes_data_12A) 
+                          nhanes_data_5A,nhanes_data_6A,nhanes_data_7A,nhanes_data_8A,nhanes_data_9A, nhanes_data_11A, nhanes_data_12A) 
   nhanes_raw_data_B <- list(nhanes_data_1B,nhanes_data_2B,nhanes_data_3B,nhanes_data_4B,
-                          nhanes_data_5B,nhanes_data_6B,nhanes_data_7B,nhanes_data_8,nhanes_data_9B,nhanes_data_10B, nhanes_data_11B, nhanes_data_12B) 
+                          nhanes_data_5B,nhanes_data_6B,nhanes_data_7B,nhanes_data_8B,nhanes_data_9B, nhanes_data_11B, nhanes_data_12B) 
   
   nhanes_raw_data_C <- data.frame(reduce(nhanes_raw_data_C, full_join, by='SEQN'))
   nhanes_raw_data_A <- data.frame(reduce(nhanes_raw_data_A, full_join, by='SEQN'))
@@ -75,10 +68,39 @@
   nhanes_raw_data <- rbind(nhanes_raw_data_C, nhanes_raw_data_A, nhanes_raw_data_B)
 
 
-# Filter and Adjust -------------------------------------------------------
+# Clearance, Imputation and Filter -------------------------------------------------------
+
+## Cleaning Before Imputing, ifelse won't change NAs
+  # matching race to CHS categories
+  nhanes_raw_data$race <- ifelse(nhanes_raw_data$RIDRETH1 == 4, 2, 1)
+  # compiling vascular history variable
+  nhanes_raw_data$vascular_history <- ifelse(nhanes_raw_data$MCQ160D == 1 | nhanes_raw_data$MCQ160F == 1 | nhanes_raw_data$MCQ160E == 1, 1, 0)
+  # compiling diabetes rollup variable
+  nhanes_raw_data$diabetes_rollup <- ifelse(nhanes_raw_data$DIQ010 == 1 | nhanes_raw_data$LBXGH > 6.5, 1, 0)
+  # compile present smoker variable to match CHS categories
+  nhanes_raw_data$SMQ040 <- ifelse(nhanes_raw_data$SMQ040 == 1 | nhanes_raw_data$SMQ040 == 2, 1, 0)
+  # convert gender to factor variable(originally: male - 1, female - 2), same as CHS
+  nhanes_raw_data$RIAGENDR <- nhanes_raw_data$RIAGENDR - 1
+  nhanes_raw_data$male = ifelse(nhanes_raw_data$RIAGENDR == 1, 0, 1)
+  # nhanes_raw_data$SBPtxt = as.factor(ifelse(nhanes_raw_data$RXDRSC1 == "I10" | nhanes_raw_data$RXDRSC1 == "I10.P", 1, 0))
+  nhanes_raw_data$SBPtxt = ifelse(nhanes_raw_data$BPQ050A == 1, 1, 0)
   
-## Filter the data to only respondents over 40 years old and < 80 yrs old
-## NHANES age: 0 to 79, 80 and over
+  ## Drop variables
+  nhanes_raw_data = nhanes_raw_data %>% select(-LBXGH, -DIQ010, -MCQ160D, -MCQ160F, -MCQ160E, -RIDRETH1, -BPQ050A)
+  
+## Imputing data
+  
+  ## Calculating missingness in NHANES data
+  missingness = (colMeans(is.na(nhanes_raw_data)))*100
+  
+  init = mice(nhanes_raw_data, maxit=0) 
+  predM = init$predictorMatrix
+  predM[,"SEQN"]=0
+  nhanes_imputed_test <- mice(nhanes_raw_data, predictorMatrix = predM, m = 5, seed = 11)
+  nhanes_raw_data <- complete(nhanes_imputed_test)
+  
+  ## Filter the data to only respondents over 40 years old and < 80 yrs old
+  ## NHANES age: 0 to 79, 80 and over
   nhanes_raw_data <- nhanes_raw_data[which(nhanes_raw_data$RIDAGEYR >= 40 
                                            & nhanes_raw_data$RIDAGEYR <= 79
                                            & nhanes_raw_data$LBXTC >= 30 
@@ -86,66 +108,36 @@
                                            & nhanes_raw_data$LBDHDD >= 5 
                                            & nhanes_raw_data$LBDHDD <= 200 
                                            & nhanes_raw_data$BPXSY1 >= 60 
-                                           & nhanes_raw_data$BPXSY1 <= 200), ]
+                                           & nhanes_raw_data$BPXSY1 <= 200
+                                           & nhanes_raw_data$vascular_history == 0), ]
+  
   # check sample size now
   nrow(nhanes_raw_data)
-
-## Calculating missingness in NHANES data
-  missingness = (colMeans(is.na(nhanes_raw_data)))*100
   
-## Imputing data
-  nhanes_raw_data$SMQ040 <- ifelse(nhanes_raw_data$SMQ040 == 0 | is.na(nhanes_raw_data$SMQ040), 0, 1)
-  nhanes_raw_data$BPQ050A <- ifelse(nhanes_raw_data$BPQ050A == 0 | is.na(nhanes_raw_data$BPQ050A), 0, 1)
-  
-  init = mice(nhanes_raw_data, maxit=0) 
-  predM = init$predictorMatrix
-  predM[,"SEQN"]=0
-  nhanes_imputed_test <- mice(nhanes_raw_data, predictorMatrix = predM, m = 5, seed = 11)
-  nhanes_raw_data <- complete(nhanes_imputed_test)
-  # compiling vascular history rollup variable
-  nhanes_raw_data$vascular_history <- as.factor(ifelse(nhanes_raw_data$MCQ160D == 1 | nhanes_raw_data$MCQ160F == 1 | nhanes_raw_data$MCQ160E == 1, 1, 0))
-
-## Sample with replacement
-  nhanes_sample = sample_n(nhanes_raw_data[!is.na(nhanes_raw_data$WTMEC2YR) & nhanes_raw_data$vascular_history == 0,], size = 100000, replace = TRUE, weight = subset(nhanes_raw_data, !is.na(WTMEC2YR) & vascular_history == 0)$WTMEC2YR)
-  
-## Data Cleaning
-  # matching race to CHS categories
-  nhanes_sample$race <- as.factor(ifelse(nhanes_sample$RIDRETH1 == 4, 2, 1))
-  # compile present smoker variable to match CHS categories
-  nhanes_sample$SMQ040 <- as.factor(ifelse(nhanes_sample$SMQ040 == 1 | nhanes_sample$SMQ040 == 2, 1, 0))
-  # convert gender to factor variable(originally: male - 1, female - 2)
-  nhanes_sample$RIAGENDR <- as.factor(nhanes_sample$RIAGENDR - 1)
-  nhanes_sample$male = ifelse(nhanes_sample$RIAGENDR == 1, 0, 1)
-  # convert ICD medicine I10/I10.P to be SBP treatment
-
-#convert race back to a 1/0 variable
-  nhanes_sample$race <- as.factor(ifelse(nhanes_sample$race == 1, 0, 1))
-
-# Convert all factor columns in the data frame to numeric
-  nhanes_sample[] <-  lapply(nhanes_sample, function(x) {
-    if(is.factor(x)) as.numeric(as.character(x)) else x
-  })
-
-# Pooled Cohort Equation --------------------------------------------------
+# Pooled Cohort Equation and Sample --------------------------------------------------
   
   source('pcr.R')
   
-  nhanes_sample = nhanes_sample %>%
-    rowwise() %>%
-    mutate(
-      PRS = rnorm(1,0,1),
-      pce_orig = pcr(gender = ifelse(male == 1, 'M', 'F'),
-                     age = RIDAGEYR,
-                     race = ifelse(race == 1, 'African American', 'Other'), 
-                     tot_chol = LBXTC,
-                     hdl_chol = LBDHDD,
-                     systolic_bp = BPXSY1,
-                     bp_treatment = (BPQ050A == 1),
-                     smoker = (SMQ040 == 1),
-                     diabetic = (DIQ010 == 1),
-                     prs_z = PRS)[[1]]/100,
-           pce_prs = 1 - exp(log(1 - pce_orig) * 1.73^PRS))
+  nhanes_raw_data$pce_orig <- sapply(1:nrow(nhanes_raw_data), function(r) {pcr(gender = ifelse(nhanes_raw_data$male[r] == 1, 'M', 'F'),
+                   age = nhanes_raw_data$RIDAGEYR[r],
+                   race = ifelse(nhanes_raw_data$race[r] == 2, 'African American', 'Other'), 
+                   tot_chol = nhanes_raw_data$LBXTC[r],
+                   hdl_chol = nhanes_raw_data$LBDHDD[r],
+                   systolic_bp = nhanes_raw_data$BPXSY1[r],
+                   bp_treatment = (nhanes_raw_data$SBPtxt[r] == 1),
+                   smoker = (nhanes_raw_data$SMQ040[r] == 1),
+                   diabetic = (nhanes_raw_data$diabetes_rollup[r] == 1),
+                   prs_z = 0)[[1]]/100})
+
+## Sample with replacement
+  nhanes_sample = sample_n(nhanes_raw_data[!is.na(nhanes_raw_data$WTMEC2YR),], size = 1000000, replace = TRUE, weight = na.omit(nhanes_raw_data$WTMEC2YR))
   
-  
+  nhanes_sample$PRS = rnorm(nrow(nhanes_sample), 0, 1)
+  nhanes_sample$pce_prs = 1 - exp(log(1 - nhanes_sample$pce_orig) * 1.73^nhanes_sample$PRS)
+
+#convert race back to a 1/0 variable
+  nhanes_sample$race <- ifelse(nhanes_sample$race == 1, 0, 1)
+
 # Save baseline pop
   save(nhanes_raw_data, nhanes_sample, file = "basepop.RData")
+  
